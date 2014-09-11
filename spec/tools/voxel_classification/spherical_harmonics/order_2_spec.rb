513e5020c38e2358cde52e243fab9ea9abc763c9
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-09-11 13:39:29 -0300

    Order 2 spherical harmonics with Complex values

--- spec/tools/voxel_classification/spherical_harmonics/order_2_spec.rb
+++ spec/tools/voxel_classification/spherical_harmonics/order_2_spec.rb
@@ -4,32 +4,32 @@ describe DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2 do
   let(:acquisition_direction){ FactoryGirl.build(:acquisition_direction) }
 
   describe 'y_2_m2' do
-    it 'is expected to return a Float' do
-      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_m2(acquisition_direction)).to be_a(Float)
+    it 'is expected to return a Complex' do
+      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_m2(acquisition_direction)).to be_a(Complex)
     end
   end
 
   describe 'y_2_m1' do
-    it 'is expected to return a Float' do
-      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_m1(acquisition_direction)).to be_a(Float)
+    it 'is expected to return a Complex' do
+      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_m1(acquisition_direction)).to be_a(Complex)
     end
   end
 
   describe 'y_2_0' do
-    it 'is expected to return a Float' do
-      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_0(acquisition_direction)).to be_a(Float)
+    it 'is expected to return a Complex' do
+      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_0(acquisition_direction)).to be_a(Complex)
     end
   end
 
   describe 'y_2_1' do
-    it 'is expected to return a Float' do
-      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_1(acquisition_direction)).to be_a(Float)
+    it 'is expected to return a Complex' do
+      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_1(acquisition_direction)).to be_a(Complex)
     end
   end
 
   describe 'y_2_2' do
-    it 'is expected to return a Float' do
-      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_2(acquisition_direction)).to be_a(Float)
+    it 'is expected to return a Complex' do
+      expect(DTITools::Tools::VoxelClassification::SphericalHarmonics::Order2.y_2_2(acquisition_direction)).to be_a(Complex)
     end
   end
 end
