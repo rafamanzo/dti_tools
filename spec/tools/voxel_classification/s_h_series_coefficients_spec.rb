82f0dad5118ee9635bb378172969c76d4b14b4a7
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-09-16 10:52:17 -0300

    Better unit tests for SHS

--- spec/tools/voxel_classification/s_h_series_coefficients_spec.rb
+++ spec/tools/voxel_classification/s_h_series_coefficients_spec.rb
@@ -8,5 +8,10 @@ describe DTITools::Tools::VoxelClassification::SHSeriesCoefficients do
     it 'is expected to return a Matrix' do
       expect(series.coefficients([1])).to be_a(Matrix)
     end
+
+    it 'is expected to return a Matrix with just one row and column' do
+      expect(series.coefficients([1]).row_count).to eq(1)
+      expect(series.coefficients([1]).column_count).to eq(1)
+    end
   end
 end
